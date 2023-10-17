a72902cac8a50b611f9cb0c2564153852610b900
Santiago Daglio <santidaglio@gmail.com>
2023-10-17 10:04:53 -0300

    agrego logica string invertido

--- bootcamp/src/main/java/com/awsoftware/bootcamp/clase3/StringInvertido.java
+++ bootcamp/src/main/java/com/awsoftware/bootcamp/clase3/StringInvertido.java
@@ -1,11 +1,27 @@
 package com.awsoftware.bootcamp.clase3;
 
+import java.util.Scanner;
+
+//Recursión: Método que recibe un String y lo devuelve invertido.
 public class StringInvertido {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        String input;
 
+        System.out.println("Ingrese una palabra: ");
+        input = sc.next();
 
+        System.out.println(cadenaInvertida(input));
+
+    }
+
+    public static String cadenaInvertida(String input){
+        if (input.length() == 1 || input.isEmpty())
+            return input;
+        else
+            return cadenaInvertida(input.substring(1)) + input.charAt(0);
     }
 
 }
