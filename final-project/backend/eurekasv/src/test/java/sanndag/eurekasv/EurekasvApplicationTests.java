8a491c4595494170d7791a70bc4c1aa1df197bb7
Santiago Daglio <santidaglio@gmail.com>
2024-03-06 03:05:43 -0300

    adding eureka sv

---
+++ final-project/backend/eurekasv/src/test/java/sanndag/eurekasv/EurekasvApplicationTests.java
@@ -0,0 +1,13 @@
+package sanndag.eurekasv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekasvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
